e612a34f725d64722aed1eaf51b913a627e69321
Github <zakaria.addi@gmail.com>
2025-12-19 13:51:04 +0100

    add header

--- ft_pipex.h
+++ ft_pipex.h
@@ -1,16 +1,33 @@
-#include "libft/libft.h"
-#include "libftprintf/ft_printf.h"
-#include <errno.h>
-#include <stdio.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipex.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaddi <zaddi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/19 13:49:25 by zaddi             #+#    #+#             */
+/*   Updated: 2025/12/19 13:50:49 by zaddi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define FILE1 1
-#define FILE2 4
-#define CMD1 2
-#define CMD2 3
+#ifndef FT_PIPEX_H
+# define FT_PIPEX_H
+
+# include "ft_printf.h"
+# include "libft.h"
+# include <errno.h>
+# include <stdio.h>
+# include <unistd.h>
+
+# define FILE1 1
+# define FILE2 4
+# define CMD1 2
+# define CMD2 3
 
 char	*ft_joinpath(char *s1, char *s2);
 char	*search_in_path(char *bin, char **path_env);
 char	**ft_find_path_env(char **envp);
 char	*get_bin_fullpath(char *bin, char **envp);
 void	ft_free_array(char **arr);
+
+#endif FT_PIPEX_H
